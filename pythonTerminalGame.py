17d1022b382a393be6325aaae0db1be86d8917d9
StevenMetz <stevenm.codes@gmail.com>
2023-07-14 18:35:14 -0500

    Finished Mortgage Calculator

--- pythonTerminalGame.py
+++ pythonTerminalGame.py
@@ -8,20 +8,19 @@ class MortgageCalculator:
     name = "Mortgage Calculator"
 
     def ask_questions(self):
-        self.name = input('Whats your name?')
-        self.principle = int(input("How much are you looking to spend?"))
+        self.name = input('Whats your name? ')
+        self.principle = int(input("How much are you looking to spend? "))
         self.years = int(
-            input("How many years will this mortgage be? I.E 15/20/30 years"))
-        self.interest_rate = int(input("What is your interest rate?")) / 100
-
-    # def __init__(self, principle, interest_rate, years, name=''):
-    #     self.principle = principle
-    #     self.interest_rate = interest_rate / 100
-    #     self.years = years
-    #     self.name = name
+            input("How many years will this mortgage be? I.E 15/20/30 years "))
+        self.interest_rate = int(
+            input("What is your interest rate? ").replace('%', '')) / 100
 
     def __repr__(self):
-        return f"Welcome {self.name} to Mortgage Calculator with a Mortgage of ${self.to_money(self.total_payed())} and an interest rate of {self.interest_rate * 100}% paying ${self.to_money(self.calculate_mortgage())} per month for {self.years} years resulting in paying ${self.to_money(self.interest_payed())} in interest"
+        return f"Welcome {self.name} to the Mortgage Calculator!\n" \
+               f"You have a mortgage of ${self.to_money(self.principle)}.\n" \
+               f"The interest rate is {self.interest_rate * 100}%.\n" \
+               f"You will pay ${self.to_money(self.calculate_mortgage())} per month for {self.years} years.\n" \
+               f"In total, you will pay ${self.to_money(self.total_payed())} which includes ${self.to_money(self.interest_payed())} in interest."
 
     def to_money(self, input):
         return "{:,.2f}".format(input)
@@ -36,16 +35,24 @@ class MortgageCalculator:
         return mortgage
 
     def total_payed(self):
-        mortgage = self.calculate_mortgage() * 360
+        mortgage = self.calculate_mortgage() * (self.years * 12)
         return mortgage
 
     def interest_payed(self):
         return self.total_payed() - self.principle
 
+    def start(self):
+        # Main program loop
+        while True:
+            self.ask_questions()
+            print(repr(self))
+            restart = input("\nWould you like to start again? (yes/no): ")
+            if restart.lower() != "yes":
+                break
+
 
 print(word)
 
 mortgage = MortgageCalculator()
-mortgage.ask_questions()
-print(mortgage)
+mortgage.start()
 # Mortage math is principal*Interest(1+interest)^number of payments / (1+r)^n - 1
